7fe4810df84649291f28530041912a1727e551f7
Maksim Borodov <mborodov@gmail.com>
2015-08-20 17:33:35 +0400

    Change dotfiles

--- .bash/aliases.sh
+++ .bash/aliases.sh
@@ -2,14 +2,22 @@
 alias modman-php="php ~/Documents/tools/modman-php/modman.php" # Change on you path for modman-php
 alias mcc="n98-magerun.phar cache:clean; n98-magerun.phar cache:flush"
 
-# Reload zsh config
-alias reload=". ~/.zshrc && echo 'ZSH config reloaded from ~/.zshrc'"
-
-# System aliases
-alias tf='tail -F -n200'
+# Management
+alias dots="cd ~/.dotfiles && sublime"
+alias reload='source ~/.bash_profile && echo "sourced ~/.bash_profile"'
+alias redot='cd ~/.dotfiles && gpp && rake install; cd -'
+alias hosts='sudo nano /etc/hosts'
 alias zshconfig='nano ~/.zshrc'
-alias top='top -o%CPU'
+
+# Shell
+alias la='ls -alh'
+alias top='top -ocpu'
+alias tf='tail -F -n200'
 alias c='clear'
+
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
 
 # Show external ip
 alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
